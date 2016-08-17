d3aa2709ef6401db09c85b8b416487631c3c8eec
WilfredKesh <wilfredgish5@gmail.com>
2016-08-16 23:08:11 -0400

    installed build dependancies

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+
+const webpack = require('webpack');
+const fs      = require('fs');
+const path    = require('path'),
+      join    = path.join,
+      resolve = path.resolve;
+
+const getConfig = require('hjs-webpack'); 
+var config = getConfig({
+  in: join(__dirname, 'src/app.js'),
+  out: join(__dirname, 'dist'),
+  clearBeforeBuild: true
+})
+module.exports = config;
